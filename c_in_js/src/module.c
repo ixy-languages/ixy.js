104ab4d90e424deb35c1c414afb5144339acf9fa
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-11-08 16:24:08 +0100

    memory.c now gets input from js
    at least i hope it does

--- c_in_js/src/module.c
+++ c_in_js/src/module.c
@@ -36,9 +36,33 @@ napi_value MempoolTest(napi_env env, napi_callback_info info)
 {
   napi_status status;
   napi_value returnVal;
-  struct mempool *myMempool = memory_allocate_mempool(36, 256);
-  status = napi_create_arraybuffer(env, myMempool->buf_size, (void **)&myMempool, &returnVal);
 
+  size_t argc = 1;
+  int num_entries = 1; // default to 1 i guess
+  int entry_size = 0;  // will default inside memory.c if it stays 0
+  napi_value argv[2];
+
+  status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Failed to parse arguments");
+  }
+
+  status = napi_get_value_int32(env, argv[0], &num_entries);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Invalid number was passed as argument");
+  }
+
+  status = napi_get_value_int32(env, argv[1], &entry_size);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Invalid number was passed as argument");
+  }
+
+  struct mempool *myMempool = memory_allocate_mempool(num_entries, entry_size);
+
+  status = napi_create_arraybuffer(env, myMempool->buf_size, (void **)&myMempool, &returnVal);
   if (status != napi_ok)
   {
     napi_throw_error(env, NULL, "Unable to create return value");
@@ -64,7 +88,7 @@ napi_value Init(napi_env env, napi_value exports)
     napi_throw_error(env, NULL, "Unable to populate exports");
   }
 
-  //adding my tets buffer thingy
+  //adding my test buffer thingy
   status = napi_create_function(env, NULL, 0, MempoolTest, NULL, &fn);
   if (status != napi_ok)
   {
--- c_in_js/testapp.js
+++ c_in_js/testapp.js
@@ -2,6 +2,11 @@ const addon = require('./build/Release/exported_module');
 // testing the random function that uses c 
 const value = 99558;
 console.log(`Input :${value}\nOutput:${addon.my_function(value)}`); // expected to add 200
-let myArrayBuffer = addon.mempoolTest();
+let numEntries = 2,
+    entrySize = 2048;
+let myArrayBuffer = addon.mempoolTest(numEntries, entrySize);
 // let's see how big this buffer is, shall we?
-console.log(`My Array buffer byte length is: ${myArrayBuffer.byteLength}`);
+console.log(`My Array buffer paramteres:
+Num Entries: ${numEntries}
+Entry Size: ${entrySize}
+Returned buffer byte length is: ${myArrayBuffer.byteLength}`);
