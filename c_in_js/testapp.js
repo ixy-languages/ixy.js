842634fd1d59da49311fa71d2a33d3af6d4d2f7d
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-11-25 16:09:41 +0100

    testing size changes actually affecting buffer

--- c_in_js/testapp.js
+++ c_in_js/testapp.js
@@ -2,11 +2,19 @@ const addon = require('./build/Release/exported_module');
 // testing the random function that uses c 
 const value = 99558;
 console.log(`Input :${value}\nOutput:${addon.my_function(value)}`); // expected to add 200
-const numEntries = 2,
-    entrySize = 2048;
+let numEntries = 2;
+const entrySize = 2048;
 let myArrayBuffer = addon.mempoolTest(numEntries, entrySize);
 // let's see how big this buffer is, shall we?
-console.log(`My Array buffer paramteres:
+console.log(`My Array buffer parameters:
+Num Entries: ${numEntries}
+Entry Size: ${entrySize}
+Returned buffer byte length is: ${myArrayBuffer.byteLength}`);
+//with more entries?
+ numEntries = 1000;
+myArrayBuffer = addon.mempoolTest(numEntries, entrySize);
+// let's see how big this buffer is, shall we?
+console.log(`My Array buffer parameters:
 Num Entries: ${numEntries}
 Entry Size: ${entrySize}
 Returned buffer byte length is: ${myArrayBuffer.byteLength}`);
