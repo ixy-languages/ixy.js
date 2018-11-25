60e78febc5094ffe9e8a1c0e89c2debce28622bc
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-11-25 16:31:14 +0100

    debugging more

--- c_in_js/src/module.c
+++ c_in_js/src/module.c
@@ -60,7 +60,7 @@ napi_value MempoolTest(napi_env env, napi_callback_info info)
     napi_throw_error(env, NULL, "Invalid number was passed as second argument");
   }
 
-  	printf("Inside module.c:\nNum entries: "+num_entries+"\n"+"Entry size: "+ entry_size);
+  	//printf("Inside module.c:\nNum entries: "+num_entries+"\n"+"Entry size: "+ entry_size);
 
 
   struct mempool *myMempool = memory_allocate_mempool(num_entries, entry_size);
@@ -72,6 +72,41 @@ napi_value MempoolTest(napi_env env, napi_callback_info info)
   }
 
   return returnVal;
+}
+
+napi_value InputTest(napi_env env, napi_callback_info info)
+{
+  napi_status status;
+
+  size_t argc = 2;
+  int num_entries = 1; // default to 1 i guess
+  int entry_size = 0;  // will default inside memory.c if it stays 0
+  napi_value argv[2];
+
+  status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Failed to parse arguments");
+  }
+
+  status = napi_get_value_int32(env, argv[0], &num_entries);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Invalid number was passed as first argument");
+  }
+
+  status = napi_get_value_int32(env, argv[1], &entry_size);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Invalid number was passed as second argument");
+  }
+
+  //printf("Inside module.c:\nNum entries: "+num_entries+"\n"+"Entry size: "+ entry_size);
+
+
+  
+
+  return argv;
 }
 
 napi_value Init(napi_env env, napi_value exports)
@@ -99,6 +134,19 @@ napi_value Init(napi_env env, napi_value exports)
   }
 
   status = napi_set_named_property(env, exports, "mempoolTest", fn);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Unable to populate exports");
+  }
+
+  // adding input test to export
+  status = napi_create_function(env, NULL, 0, InputTest, NULL, &fn);
+  if (status != napi_ok)
+  {
+    napi_throw_error(env, NULL, "Unable to wrap native function");
+  }
+
+  status = napi_set_named_property(env, exports, "InputTest", fn);
   if (status != napi_ok)
   {
     napi_throw_error(env, NULL, "Unable to populate exports");
