1da64498ab7566d989ade1d89dae248cfb17f6c2
Sebastian Di Luzio <sebidiluzio@gmail.com>
2019-04-17 10:50:48 +0200

    remove testing code

--- c_in_js/src/module.c
+++ c_in_js/src/module.c
@@ -142,24 +142,6 @@ napi_value getIDs(napi_env env, napi_callback_info info)
 
 int regUsed = IXGBE_LEDCTL;
 
-/*int getAddress(char *reg)
-{
-  switch (reg)
-  {
-  case "EIMC":
-    return (int)IXGBE_EIMC;
-  case "EIAC":
-    return (int)IXGBE_EIAC;
-  case "EIAM":
-    return (int)IXGBE_EIAM;
-  case "EITR":
-    return (int)IXGBE_EITR;
-  case "EICR":
-    return (int)IXGBE_EICR;
-  default:
-    return (int)0x00000;
-  }
-}*/
 // tmp copypastas
 bool turnoffRMDr = false;
 bool turnoffEBLDMA = true;
@@ -202,50 +184,9 @@ void enable_dma(const char *pci_addr)
     check_err(close(fd), "close");
   }
 }
-uint8_t *pauls_pci_map_resource(const char *pci_addr)
-{
-  char path[PATH_MAX];
-  snprintf(path, PATH_MAX, "/sys/bus/pci/devices/%s/resource0", pci_addr);
-  debug("Mapping PCI resource at %s", path);
-  remove_driver(pci_addr);
-  enable_dma(pci_addr);
-  int fd = check_err(open(path, O_RDWR), "open pci resource");
-  debug("pauls fd: %d", fd);
-  struct stat stat;
-  check_err(fstat(fd, &stat), "stat pci resource");
-  return (uint8_t *)check_err(mmap(NULL, stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0), "mmap pci resource");
-}
-void pauls_set_reg32(uint8_t *addr, int reg, uint32_t value)
-{
-  __asm__ volatile(""
-                   :
-                   :
-                   : "memory");
-  *((volatile uint32_t *)(addr + reg)) = value;
-}
-//endof copypastas
-// lets try to make this work in JS:
-/*set_reg32(dev->addr, IXGBE_EIMC, 0x7FFFFFFF);
-  defined as:
-  static inline void set_reg32(uint8_t *addr, int reg, uint32_t value)
-{
-	__asm__ volatile(""
-					 :
-					 :
-					 : "memory");
-	*((volatile uint32_t *)(addr + reg)) = value;
-}
-*/
 
-/**
- * Set a register
- * @param $addr
- * address of the memory we want to address, should be the root address of the ixy device
- * @param $reg
- * the register we want to write to, being an offset in bytes from addr
- * @param $value
- * The value we want to write into the register
- * */
+//endof copypastas
+
 void set_reg(uint8_t *addr, int32_t reg, uint32_t value)
 {
   printf("We got input addr: %d, reg: %d, value: %d\n", addr, reg, value);
@@ -261,6 +202,7 @@ void *get_reg(uint8_t *addr, int reg)
   return (addr + reg);
 }
 
+// let's keep this for debugging purposes
 napi_value printBits(napi_env env, napi_callback_info info)
 {
   napi_status stat;
@@ -298,13 +240,9 @@ napi_value printBits(napi_env env, napi_callback_info info)
   printf("Size of the stat: %d\n", stat2.st_size);
 
   uint8_t *pci_map_resource_js = check_err(mmap(NULL, stat2.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0), "mmap pci resource");
-  // uint8_t *filepointer = pci_map_resource_js;
   //printf("should be 0x00800 : %x", getAddress(regi));
   uint8_t *filepointer = get_reg(pci_map_resource_js, regUsed);
 
-  //uint8_t *pauls_filepointer = pauls_pci_map_resource(pci_addr); //according to below code we get the same data
-  //pauls_filepointer += regUsed;                                  //shifting pointer
-  //printf("%d ; our filepointer\n%d ; pauls filepointer\n", filepointer, pauls_filepointer);
   printf("%d :: our resource at 0x%x\n", filepointer[0], regUsed);
   printf("%x", filepointer[0]);
   SHOW(uint8_t, filepointer[0]);
@@ -316,25 +254,7 @@ napi_value printBits(napi_env env, napi_callback_info info)
   uint32_t *bitFP2 = filepointer;
   printf("%x", bitFP2[0]);
   SHOW(uint32_t, bitFP2[0]);
-  //printf("changing values with pauls function set_reg...\n");
-  //pauls_set_reg32(pci_map_resource_js, regUsed, 32);
 
-  // compare to what pauls code got
-  // loop vars:
-  /*
-  int i = 0;
-  int offset = 0;
-  int lengthofloop = 16;
-
-  for (i = offset; i < lengthofloop + offset; i += 1)
-  {
-    printf("%d\n", i);
-    printf("    ");
-    SHOW(uint8_t, filepointer[i]);
-    SHOW(uint8_t, pauls_filepointer[i]);
-  }
-*/
-  //void *filepointer = pci_map_resource_js;
   napi_value testReturnVal;
   stat = napi_create_external_arraybuffer(env, (void *)filepointer, stat2.st_size, NULL, NULL, &testReturnVal);
   if (stat != napi_ok)
@@ -344,8 +264,6 @@ napi_value printBits(napi_env env, napi_callback_info info)
   return testReturnVal;
 }
 // endof trying
-
-// here should be clean code
 
 /**
  * This will give us an ArrayBuffer in JS, which points to the Network Card installed in the PCI Address we provide
@@ -429,244 +347,12 @@ napi_value set_reg_js(napi_env env, napi_callback_info info)
 
   return NULL;
 }
-//endof clean code
-napi_value getReg(napi_env env, napi_callback_info info)
-{
-  napi_status stat;
-  size_t argc = 2;
-  napi_value argv[2];
-
-  stat = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
-  if (stat != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Failed to parse arguments");
-  }
-  char *pci_addr = malloc(12); // "0000:03:00.0"
-  size_t size;
-  stat = napi_get_value_string_utf8(env, argv[0], pci_addr, 13, &size); // for some reason we need to use length 13 not 12, to get 12 bytes
-  if (stat != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Invalid string of length 12, the PCI adress, was passed as first argument");
-  }
-  bool onlyReadPlease = false;
-  stat = napi_get_value_bool(env, argv[1], &onlyReadPlease);
-  if (stat != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Failed to get the 2nd argument, a boolean");
-  }
-  debug("read only : %d", onlyReadPlease);
-
-  remove_driver(pci_addr); // we added this to see if it works now
-  enable_dma(pci_addr);    // do we need this to actually be able to write there?
-
-  //this is what we need to get the root adress
-  int fd = pci_open_resource(pci_addr, "resource0");
-  struct stat stat2;
-  check_err(fstat(fd, &stat2), "stat pci resource");
-  printf("Size of the stat: %d\n", stat2.st_size);
-
-  //this needs to be fixed:
-  uint8_t *pci_map_resource_js = check_err(mmap(NULL, stat2.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0), "mmap pci resource");
-  // uint8_t *filepointer = pci_map_resource_js;
-  uint8_t *filepointer = get_reg(pci_map_resource_js, regUsed);
-  uint8_t *filepointerUint8 = filepointer;
-
-  if (!onlyReadPlease)
-  { // i only want this printed once
-    //or not at all, because it ist 52k characters long
-    //debug("filepointer: %s", filepointer);
-  }
-  // loop vars:
-  int i = 0;
-  int offset = 0;
-  int lengthofloop = 16;
-
-  for (i = offset; i < lengthofloop + offset; i += 1)
-  {
-    printf("%d :: our resource at uint8 %d\n", filepointer[i], i);
-    SHOW(uint8_t, filepointer[i]);
-  }
-  if (!onlyReadPlease)
-  {
-    int valueChanger = filepointer[0] + 1;
-    debug("setting byte 0 to %d .", valueChanger);
-    filepointer[0] = valueChanger;
-    uint8_t changedInt = filepointer[0];
-    uint8_t changed8bitInt = filepointerUint8[0];
-    printf("the changed value directly after being changed: %d ; the uint8 value: %d\n", changedInt, changed8bitInt);
-    SHOW(uint8_t, changedInt);
-    printf("filepointer at 0 now : %d\n", filepointer[0]);
-    debug("testing change and instant print, without saving to another variable (i fear the compiler might be tricking me)\n");
-    filepointer[0] = valueChanger;
-    debug("%d", filepointer[0]);
-
-    printf("below should look the same:\n");
-    for (i = offset; i < lengthofloop + offset; i += 1)
-    {
-      printf("%d :: our resource at uint8 %d\n", filepointer[i], i);
-      SHOW(uint8_t, filepointer[i]);
-    }
-    for (i = offset; i < lengthofloop + offset; i += 1)
-    {
-      filepointer[i] = i + 4;
-      printf("just changed value at %d to %d: changed value: %d\n", i, i + 4, filepointer[i]);
-    }
-    debug("just printing the same again...");
-    for (i = offset; i < lengthofloop + offset; i += 1)
-    {
-      printf("%d :: our resource at uint8 %d\n", filepointer[i], i);
-      SHOW(uint8_t, filepointer[i]);
-    }
-    debug("reloading the same area to see if the change persisted");
-    //pci_map_resource_js = check_err(mmap(NULL, stat2.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0), "mmap pci resource"); // we get the error Invalid Argument here
-    filepointer = get_reg(pci_map_resource_js, regUsed);
-    //filepointer = pci_map_resource_js;
-
-    for (i = offset; i < lengthofloop + offset; i += 1)
-    {
-      printf("%d :: our resource at uint8 %d\n", filepointer[i], i);
-      SHOW(uint8_t, filepointer[i]);
-    }
-  }
-  //void *filepointer = pci_map_resource_js;
-  napi_value testReturnVal;
-  stat = napi_create_external_arraybuffer(env, (void *)filepointer, stat2.st_size, NULL, NULL, &testReturnVal);
-  if (stat != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Failed our external buffer creation");
-  }
-  return testReturnVal;
-}
-// Try writing a string into the buf (WORKS finally!)
-napi_value writeString(napi_env env, napi_callback_info info)
-{
-
-  printf("c says is this little endian?: %d\n", isLittleEndian());
-  napi_status status;
-  napi_value returnVal;
-
-  size_t argc = 2;
-  napi_value argv[2];
-
-  status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Failed to parse arguments");
-  }
-  char inputString[8];
-  status = napi_get_value_string_utf8(env, argv[0], inputString, sizeof(inputString), NULL);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Invalid string of length 10 was passed as first argument");
-  }
-  bool result;
-  status = napi_is_arraybuffer(env, argv[1], &result);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Failed checking if input is arraybuffer");
-  }
-  printf("The inputted data is an arraybuffer?: %d\n", result);
-  //char string[10];
-  int16_t *inputArrayBuffer[4];
-  size_t lengthOfString;
-  status = napi_get_arraybuffer_info(env,
-                                     argv[1],
-                                     (void **)inputArrayBuffer,
-                                     &lengthOfString);
-  // napi_get_value_string_utf8(env, argv[1], string, 10, NULL);
-
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Invalid arraybuffer was passed as second argument");
-  }
-  printf("The inputted arraybuffer as string: %s\n", argv[1]);
-  printf("The inputted arraybuffers data as string: %s\n", inputArrayBuffer);
-
-  printf("length of arraybuffer: %d\n", lengthOfString);
-  // printf("Input string: %s\n", inputString);
-  // printf("Input arraybuffer: %d\n", argv[1]);
-  printf("size of a single element in arraybuffer: %d\n", sizeof(inputArrayBuffer[0][0]));
-  for (int i = 0; i < lengthOfString / sizeof(inputArrayBuffer[0][0]); i++)
-  {
-    printf("Input arraybuffers data at index %d : %d\n", i, inputArrayBuffer[0][i]);
-    inputArrayBuffer[0][i] += 30;
-    printf("Change arraybuffers data at index %d : %d\n", i, inputArrayBuffer[0][i]);
-  }
-
-  return argv[1]; // this should be the original array buffer, and we changed the data lying beneath? - yes
-}
-
-napi_value arrayTest(napi_env env, napi_callback_info info) // we create a uint32 array based on an input, to be sure we deliver data correctly
-{
-  napi_status status;
-  napi_value ret;
-  uint32_t *array = malloc(4 * sizeof(uint32_t));
-  array[0] = 7;
-  uint32_t *uints;
-  debug("array at 0: %d, array at 1: %d", array[0], array[1]);
-
-  // trying to create an array buffer from this input
-  status = napi_create_external_arraybuffer(env,
-                                            array,
-                                            4 * sizeof(uint32_t),
-                                            NULL,
-                                            NULL,
-                                            &ret);
-
-  /*status = napi_create_arraybuffer(env,
-                                   4 * sizeof(uint32_t),
-                                   &uints,
-                                   &ret);*/
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to create return value");
-  }
-  //uints = array;
-
-  return ret;
-}
-
-napi_value readArray(napi_env env, napi_callback_info info)
-{
-  napi_status status;
-  size_t argc = 1;
-  napi_value argv[1];
-
-  status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Failed to parse arguments");
-  }
-
-  uint32_t *array;
-  size_t size;
-
-  status = napi_get_arraybuffer_info(env, argv[0], &array, &size);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "failed to load the array buffer into C");
-  }
-  debug("array at 0: %d, array at 1: %d", array[0], array[1]);
-  return NULL;
-}
 
 napi_value Init(napi_env env, napi_value exports)
 {
   napi_status status;
   napi_value fn;
 
-  //adding my test string thingy
-  status = napi_create_function(env, NULL, 0, writeString, NULL, &fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to wrap native function");
-  }
-
-  status = napi_set_named_property(env, exports, "writeString", fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to populate exports");
-  }
   //adding my 32 bit print function
   status = napi_create_function(env, NULL, 0, printBits, NULL, &fn);
   if (status != napi_ok)
@@ -675,18 +361,6 @@ napi_value Init(napi_env env, napi_value exports)
   }
 
   status = napi_set_named_property(env, exports, "printBits", fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to populate exports");
-  }
-  //adding the read array
-  status = napi_create_function(env, NULL, 0, readArray, NULL, &fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to wrap native function");
-  }
-
-  status = napi_set_named_property(env, exports, "readArray", fn);
   if (status != napi_ok)
   {
     napi_throw_error(env, NULL, "Unable to populate exports");
@@ -703,33 +377,7 @@ napi_value Init(napi_env env, napi_value exports)
   {
     napi_throw_error(env, NULL, "Unable to populate exports");
   }
-
-  //adding my second test buffer thingy
-  status = napi_create_function(env, NULL, 0, arrayTest, NULL, &fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to wrap native function");
-  }
-
-  status = napi_set_named_property(env, exports, "arrayTest", fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to populate exports");
-  }
-  //add getReg to exports
-  status = napi_create_function(env, NULL, 0, getReg, NULL, &fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to wrap native function");
-  }
-
-  status = napi_set_named_property(env, exports, "getReg", fn);
-  if (status != napi_ok)
-  {
-    napi_throw_error(env, NULL, "Unable to populate exports");
-  }
-
-  //add getReg to exports
+  // add getIXYAddr to the export
   status = napi_create_function(env, NULL, 0, getIXYAddr, NULL, &fn);
   if (status != napi_ok)
   {
@@ -741,7 +389,7 @@ napi_value Init(napi_env env, napi_value exports)
   {
     napi_throw_error(env, NULL, "Unable to populate exports");
   }
-  //add getReg to exports
+  // add set_reg to the export
   status = napi_create_function(env, NULL, 0, set_reg_js, NULL, &fn);
   if (status != napi_ok)
   {
